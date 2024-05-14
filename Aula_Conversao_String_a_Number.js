3c39ff825094a9ce91533bd70f55f688f1bc887b
Edmarcio Carlos Fonseca Junior <Edmarcio.junior@outlook.com>
2024-05-14 12:12:33 -0300

    Aula sobre conversão de string para Number
    Revisão de formas de conversão de variaveis.

---
+++ Aula_Conversao_String_a_Number.js
@@ -0,0 +1,30 @@
+//Static
+//C, C++, Java, TypeScript
+//Dynamic
+//Pyton, Javascript, Ruby, PHP
+
+let age = '25',
+  age_F = '25.5'
+console.log(age, typeof age)
+
+// parseInt() converte a varivei antiga para int
+age = parseInt(age)
+console.log(age, typeof age)
+
+// parseFloat() converte a varivei antiga para float
+age_F = parseFloat(age_F)
+console.log(age_F, typeof age_F)
+
+//Unario(+) exetuda tanto int como float
+age = +age
+console.log(age, typeof age)
+age_F = +age_F
+console.log(age_F, typeof age_F)
+
+//Number (Numero) exetuda tanto int como float
+age = Number(age)
+console.log(age, typeof age)
+
+/*
+ * Usar int e flot somente quando tiver certeza que os valores precisão ser assim.
+ */
