1cdab402cfa872d490a8f7393c70cfc7961c948d
Edmarcio Carlos Fonseca Junior <Edmarcio.junior@outlook.com>
2024-05-14 11:28:04 -0300

    Aula sobre variaveis
    Entendendo sobre var let e const

---
+++ Aula_Variaveis.js
@@ -0,0 +1,14 @@
+let fristName = 'Edmarcio'
+let lastName = 'Carlos'
+let age = 30
+console.log(fristName, lastName, age)
+
+const calcTax = 0.7
+console.log(calcTax)
+
+/*
+ * var A forma mais antiga de declarar variáveis tem escopo de função e pode ser redeclarada e atualizada.
+ * let permite a declaração de variáveis com escopo de bloco, o que significa que a variável só existe dentro do bloco onde foi declarada. Pode ser atualizada, mas não redeclarada no mesmo escopo.
+ * const são usadas para atribuído um valor, ele não pode ser alterado.
+ * (Dica) para valores que sabemos que não mudarão, como URLs de API, valores de configuração, etc., para garantir a imutabilidade desses valores no código.
+ */
