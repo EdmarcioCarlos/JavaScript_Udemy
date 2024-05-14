483dd26b4d56b269d68d7b1d68336117e83a475e
Edmarcio Carlos Fonseca Junior <Edmarcio.junior@outlook.com>
2024-05-14 10:46:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JavaScript_Udemy
+ Estudo sobre JavaScript
---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('Edmarcio')
+console.warn('Aviso de erro')
+console.error('404 not found')
+
+console.group('Grupo')
+console.log('Edmarcio')
+console.warn('Aviso de erro')
+console.error('404 not found')
+console.groupEnd()
+
+
+/*
+    *console.group()  - ele agrupo os consoles que você colocar ate o proximo console.groupEND*();
+    *console.log('') passa um informação para o console WEB
+    *console.warn('') Informa uma aviso ao console WEB 
+    *console.error('') Informa um erro ao console WEB
+*/
