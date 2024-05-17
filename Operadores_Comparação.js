757e7c2231d3a0d2249a179cab70a9f85bda7e2e
Edmarcio Carlos Fonseca Junior <Edmarcio.junior@outlook.com>
2024-05-17 10:03:55 -0300

    Aula Operadores de comparação
    Estudo sobre todos operadores de comparação.

---
+++ Operadores_Comparação.js
@@ -0,0 +1,15 @@
+//Operadores Comparação
+
+//igualdade Solta ((dado)
+console.log(3 == '3')
+// igualdade Estrita (dado e tipo iguais)
+console.log(3 === '3')
+// Desigualde solta (dado)
+console.log(3 != 3)
+// Desigualde Estrita (dado e tipo iguais)
+console.log(3 !== 3)
+
+console.log(8 > 5) // Maior que
+console.log(5 < 5) // Menor que
+console.log(5 >= 5) // Maior ou igual
+console.log(5 <= 5) //Menor ou igual
