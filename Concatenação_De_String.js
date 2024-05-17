a22df2f52b9ff0081f0814c01b71d06e661ec764
Edmarcio Carlos Fonseca Junior <Edmarcio.junior@outlook.com>
2024-05-17 10:13:47 -0300

    Aula sobre correção de tipo e Concatenação

---
+++ Concatenação_De_String.js
@@ -0,0 +1,8 @@
+// Concatenação de String
+
+let fristName = 'Edmarcio'
+let lastName = 'Carlos'
+console.log(fristName + ' ' + lastName)
+
+let fullName = fristName + ' ' + lastName
+console.log(fullName)
---
+++ Coreção_De_Tipo.js
@@ -0,0 +1,5 @@
+let total
+total = 3 + '5' // Converte para string
+total = '5' - '4' // Converte para number
+total = '5' * '4' // Converte para number
+total = '5' / '4' // Converte para number
