9ed87a837ec69605a92361aec3e2dd077b11c401
Edmarcio Carlos Fonseca Junior <Edmarcio.junior@outlook.com>
2024-05-17 09:50:16 -0300

    Aula sobre Operadores
    Aula sobre operadores aritimeticos e de atribuição.

---
+++ Operador_De_Atribuicao.js
@@ -0,0 +1,9 @@
+let total = 3
+total += 5 // adicionar +5
+total -= 5 // Subtratir -5
+total *= 5 // Multiplicação
+total /= 5 // Divisão
+total %= 4 // Resto
+total **= 3 // Exponenciação
+
+console.log(total)
---
+++ Operadorea_Aritmeticos.js
@@ -0,0 +1,11 @@
+// operadores aritimeticos
+let total = 6 + 6 // ADIÇÃO
+// let total = 6 - 6 // Subtração
+// let total = 6 * 6 // Multiplicação
+// let total = 6 / 6 // Divisão
+// let total = 6 % 6 // Resto da divisão
+// let total++ // Incremento
+// let total-- // Decremento
+// let total = 6**3 // Exponenciação
+
+console.log(total)
